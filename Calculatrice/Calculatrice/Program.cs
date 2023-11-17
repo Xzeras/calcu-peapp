27849037fa3dc0df86a13628ace5a380b3a616a0
Xzeras <151165344+Xzeras@users.noreply.github.com>
2023-11-17 16:00:59 +0100

    calcu.v.1 (17.11.23)

--- Calculatrice/Calculatrice/Program.cs
+++ Calculatrice/Calculatrice/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Calculatrice
 {
@@ -10,6 +12,71 @@ namespace Calculatrice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bienvenue Sur La Calculatrice"); // phrase du debut
+
+            string s;                                       // prouver que (s) est un string
+            int y = Convert.ToInt32(Console.ReadLine());    // (y) lit les premiers nombres
+            string x = Console.ReadLine();                  // (x) lit les (+ - * /) qui ont été écris
+            int e = Convert.ToInt32(Console.ReadLine());    // (e) lit les deuxiemes nombres
+            bool t = true;
+
+
+            do
+            {
+
+                if (x == "+")                                                      // si (x) lit (+) il s'active
+                {
+                    int w = y + e;
+                    Console.WriteLine();
+                    Console.WriteLine(w);
+                    Console.WriteLine("Voulez vous fair un autre calcul ?");
+                    Console.WriteLine("oui/ non");
+                }
+
+                else if (x == "-")                                                 // si (x) lit (-) il s'active
+                {
+                    int w = y - e;
+                    Console.WriteLine();
+                    Console.WriteLine(w);
+                    Console.WriteLine("Voulez vous fair un autre calcul ?");
+                    Console.WriteLine("oui/ non");
+                }
+                else if (x == "/")                                                 // si (x) lit (/) il s'active
+                {
+                    int w = y / e;
+                    Console.WriteLine();
+                    Console.WriteLine(w);
+                    Console.WriteLine("Voulez vous fair un autre calcul ?");
+                    Console.WriteLine("oui/ non");
+                }
+                else if (x == "*")                                                 // si (x) lit (*) il s'active
+                {
+                    int w = y * e;
+                    Console.WriteLine();
+                    Console.WriteLine(w);
+                    Console.WriteLine("Voulez vous fair un autre calcul ?");
+                    Console.WriteLine("oui/ non");
+                }
+
+                 s = Console.ReadLine();                                         
+                if ( s == "non")                                                   // si (s) lit (non) il s'active
+                {
+                    t = false;                                                     // active la fin de la boucle
+                }
+   
+            } while (false);
+
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+
+
         }
     }
 }
